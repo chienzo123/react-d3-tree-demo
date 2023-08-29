68fa8966af2b1b869822d97b775b3d1d9c8788be
chien-hoang <chien.hoang@monstar-lab.com>
2023-08-29 13:40:45 +0700

    add more sample

--- src/App.js
+++ src/App.js
@@ -1,84 +1,85 @@
-import React, { Component } from 'react';
-import clone from 'clone';
-import Tree from 'react-d3-tree';
-import MixedNodeElement from './components/MixedNodeElement';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import React from "react";
+import Tree from "react-d3-tree";
+import MixedNodeElement from "./components/MixedNodeElement";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
-import './App.css';
+import "./App.css";
 
 // Data examples
-import orgChartJson from './examples/org-chart.json';
+import orgChartJson from "./examples/org-chart.json";
 const customNodeFnMapping = {
   mixed: {
-    description: 'MixedNodeElement - SVG `circle` + `foreignObject` label',
+    description: "MixedNodeElement - SVG `circle` + `foreignObject` label",
     fn: ({ nodeDatum, toggleNode }, appState) => (
       <MixedNodeElement
         nodeData={nodeDatum}
         triggerNodeToggle={toggleNode}
         foreignObjectProps={{
-          width: 200,
-          height: 200,
-          x: -100,
-          y: -62,
+          width: 300,
+          height: 300,
+          x: -150,
+          y: -62
         }}
       />
-    ),
-  },
+    )
+  }
 };
 
 const App = () => {
-    const [state, setState ]= React.useState({
-      data: orgChartJson,
-      renderCustomNodeElement: customNodeFnMapping.mixed.fn,
-      styles: {
-        nodes: {
-          node: {
-            circle: {
-              fill: '#52e2c5',
-            },
-            attributes: {
-              stroke: '#000',
-            },
+  const [state, setState] = React.useState({
+    data: orgChartJson,
+    renderCustomNodeElement: customNodeFnMapping.mixed.fn,
+    styles: {
+      nodes: {
+        node: {
+          circle: {
+            fill: "#52e2c5"
           },
-          leafNode: {
-            circle: {
-              fill: 'transparent',
-            },
-            attributes: {
-              stroke: '#000',
-            },
-          },
+          attributes: {
+            stroke: "#000"
+          }
         },
-      },
-    });
+        leafNode: {
+          circle: {
+            fill: "transparent"
+          },
+          attributes: {
+            stroke: "#000"
+          }
+        }
+      }
+    }
+  });
 
-    return (
-      <div className="App">
-        <div className="demo-container">
-          <div className="column-right">
-            <div className="tree-container">
+  return (
+    <div className="App">
+      <div className="demo-container">
+        <div className="column-right">
+          <div className="tree-container">
             <DndProvider backend={HTML5Backend}>
               <Tree
                 hasInteractiveNodes
                 data={state.data}
                 renderCustomNodeElement={
                   state.renderCustomNodeElement
-                    ? rd3tProps => state.renderCustomNodeElement(rd3tProps, state)
+                    ? rd3tProps =>
+                        state.renderCustomNodeElement(rd3tProps, state)
                     : undefined
                 }
-                orientation='vertical'
+                orientation="vertical"
                 enableLegacyTransitions
-                transitionDuration={500}
-                separation= {{ siblings: 2, nonSiblings: 2 }}
+                transitionDuration={100}
+                separation={{ siblings: 2, nonSiblings: 2 }}
                 translate={{ x: 200, y: 300 }}
                 styles={state.styles}
+                nodeSize={{ x: 300, y: 300 }}
               />
             </DndProvider>
-            </div>
           </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/MixedNodeElement.js
+++ src/components/MixedNodeElement.js
@@ -1,57 +1,93 @@
-import React from 'react';
-import { useDrag, useDrop } from 'react-dnd';
+import React from "react";
+import { useDrag, useDrop } from "react-dnd";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const MixedNodeElement = ({ nodeData = {}, triggerNodeToggle, foreignObjectProps = {} }) => {
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+const card = name => {
+  return (
+    <React.Fragment>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </React.Fragment>
+  );
+};
+
+const MixedNodeElement = ({
+  nodeData = {},
+  triggerNodeToggle,
+  foreignObjectProps = {}
+}) => {
   const nodeRef = React.useRef(null);
   const [{ isDragging }, drag] = useDrag({
-    type: 'node',
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
+    type: "node",
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
     }),
     end: (a, monitor) => {
       const dropResult = monitor.getDropResult();
-      if(dropResult)
-      alert(`You moved ${nodeData.name} to ${dropResult.name}`);
+      if (dropResult) alert(`You moved ${nodeData.name} to ${dropResult.name}`);
     }
-    
   });
 
-  const [{ isOver }, drop] = useDrop({
-    accept: 'node',
-    drop: () => (
-      { name: nodeData.name }
-    ),
-    collect: (monitor) => ({
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: "node",
+    drop: () => ({ name: nodeData.name }),
+    collect: monitor => ({
       isOver: monitor.isOver(),
-    }),
+      canDrop: monitor.canDrop()
+    })
   });
+  const isActive = canDrop && isOver;
+  let backgroundColor = "white";
+  if (isActive) {
+    backgroundColor = "#ddffd2";
+  } else if (canDrop) {
+    backgroundColor = "#ffeedc";
+  }
+  const opacity = isDragging ? 0.4 : 1;
   return (
     <React.Fragment>
       <foreignObject {...foreignObjectProps}>
         <div ref={drop}>
-        <div ref={drag}>
-        <div
-        ref={nodeRef}
-        onClick={triggerNodeToggle}
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            border: '1px solid black',
-            backgroundColor: 'rgb(248, 248, 255)', // ghostwhite
-          }}
-        >
-          <h3>{nodeData.name}</h3>
-          <ul style={{ listStyleType: 'none', padding: 0 }}>
-            {nodeData.attributes &&
-              Object.keys(nodeData.attributes).map((labelKey, i) => (
-                <li key={`${labelKey}-${i}`}>
-                  {labelKey}: {nodeData.attributes[labelKey]}
-                </li>
-              ))}
-          </ul>
-        </div>
-        </div>
+          <div ref={drag}>
+            <Box sx={{ minWidth: 200 }}>
+              <Card
+                ref={nodeRef}
+                onClick={triggerNodeToggle}
+                variant="outlined"
+                style={{ cursor: "pointer", opacity, backgroundColor }}
+              >
+                {card(nodeData.name)}
+              </Card>
+            </Box>
+          </div>
         </div>
       </foreignObject>
     </React.Fragment>
